d94173aae30ef47d376c4bf79ce3d3511dbdf484
ZumTheZazaKing <zahidi85543@gmail.com>
2021-09-18 15:42:09 +0800

    Added more iamges and slowed down image speed

--- src/MainComponents/Main.js
+++ src/MainComponents/Main.js
@@ -8,6 +8,16 @@ import Image2 from '../Images/mainImage2.jpeg';
 import Image3 from '../Images/mainImage3.jpeg';
 import Image4 from '../Images/mainImage4.jpeg';
 import Image5 from '../Images/mainImage5.jpeg';
+import Image6 from '../Images/mainImage6.jpeg';
+import Image7 from '../Images/mainImage7.jpeg';
+import Image8 from '../Images/mainImage8.jpeg';
+import Image9 from '../Images/mainImage9.jpeg';
+import Image10 from '../Images/mainImage10.jpeg';
+import Image11 from '../Images/mainImage11.jpeg';
+import Image12 from '../Images/mainImage12.jpeg';
+import Image13 from '../Images/mainImage13.jpeg';
+import Image14 from '../Images/mainImage14.jpeg';
+import Image15 from '../Images/mainImage15.jpeg';
 
 import Button from '@material-ui/core/Button';
 
@@ -25,7 +35,10 @@ export function Main(){
     let [image, setImage] = useState(Image1);
     let [imageIndex, setImageIndex] = useState(0);
 
-    const images = [Image1,Image2,Image3,Image4,Image5];
+    const images = [
+        Image1,Image2,Image3,Image4,Image5,Image6,Image7,Image8,Image9,Image10,
+        Image11,Image12,Image13,Image14,Image15
+    ];
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -36,7 +49,8 @@ export function Main(){
                 setImageIndex(++imageIndex);
                 setImage(images[imageIndex]);
             }
-        },3000)
+            console.log(imageIndex)
+        },10000)
         return () => clearInterval(interval);
     });
 
