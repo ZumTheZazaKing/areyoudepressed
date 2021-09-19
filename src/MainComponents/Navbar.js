7e67d35a279b469e235ff649977d8229dbef2884
ZumTheZazaKing <zahidi85543@gmail.com>
2021-09-19 18:40:49 +0800

    Menu for mobile added

--- src/MainComponents/Navbar.js
+++ src/MainComponents/Navbar.js
@@ -1,5 +1,8 @@
 import { useHistory } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { Context } from '../Contexts/Context';
 
@@ -9,21 +12,45 @@ export function Navbar(){
 
     let { currentLanguage, setShowAbout, setShowContact } = useContext(Context);
 
+    const [mobileClass, setMobileClass] = useState("");
+
+    const showMobileMenu = () => setMobileClass("show");
+    const hideMobileMenu = () => setMobileClass("");
+
+    const toggleMobileMenu = () => {
+        mobileClass ? hideMobileMenu() : showMobileMenu();
+    }
+
+    const goHome = () => {
+        history.push("/");
+        hideMobileMenu();
+    }
+
     const goAbout = () => {
         history.push("/about");
         setShowAbout(true);
+        hideMobileMenu();
     }
 
     const goContact = () => {
         history.push("/contact");
         setShowContact(true);
+        hideMobileMenu();
     }
 
     return (<div id="Navbar">
         <h3>Are You Depressed?</h3>
         <div id="links">
-            <span onClick={() => history.push("/")}>{currentLanguage.navbar.home}</span>
+            <span onClick={() => goHome()}>{currentLanguage.navbar.home}</span>
             <span onClick={() => goAbout()}>{currentLanguage.navbar.about}</span>
+            <span onClick={() => goContact()}>{currentLanguage.navbar.contact}</span>
+            <span id="hamburger" onClick={() => toggleMobileMenu()}>{mobileClass ? <CloseIcon/> : <MenuIcon/>}</span>
+        </div>
+        <div id="mobile" className={mobileClass}>
+            <span onClick={() => goHome()}>{currentLanguage.navbar.home}</span>
+            <hr/>
+            <span onClick={() => goAbout()}>{currentLanguage.navbar.about}</span>
+            <hr/>
             <span onClick={() => goContact()}>{currentLanguage.navbar.contact}</span>
         </div>
     </div>)
